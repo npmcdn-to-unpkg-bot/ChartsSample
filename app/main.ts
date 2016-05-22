9679d00b800cf06657c6f143aba440f40f2a5b98
Artem Storozhuk <artems@earnix.com>
2016-05-22 16:10:06 +0300

    Remove .idea file and unused lines from main.ts

--- app/main.ts
+++ app/main.ts
@@ -2,10 +2,8 @@ import {bootstrap} from "@angular/platform-browser-dynamic";
 import {AppComponent} from "./app.component";
 import {ColorPickerService} from "./color-picker/color-picker.service";
 import {ChartService} from "./chart-component/chart.service.ts"
-//import {ChartGraphDirective} from "./chart-component/chart-graph-component";
 
 //import {enableProdMode} from '@angular/core'
 //enableProdMode();
 
-//bootstrap(ChartGraphDirective);
 bootstrap(AppComponent, [ColorPickerService, ChartService]);
